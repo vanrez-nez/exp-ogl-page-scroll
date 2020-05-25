c281135970343095631e13252fe9914f85cc6a41
Iván Juárez Núñez <ivan@blindgazer.com>
2020-05-24 19:25:12 -0500

    feat: repositioning scrolled elements

--- src/main.js
+++ src/main.js
@@ -4,9 +4,26 @@ import Scroller from './scroller';
 class Demo {
   constructor() {
     this.scroller = new Scroller({
-      target: document.querySelector('#js-container')
+      target: document.querySelector('#js-container'),
+      onScrollCallback: this.onScroll.bind(this),
     });
     this.canvas = new WebGLCanvas({});
+    this.canvas.addDomImages(document.querySelectorAll('img'));
+    window.addEventListener('resize', this.onResize.bind(this));
+    this.onResize();
+  }
+
+  onScroll(top) {
+    const { canvas } = this;
+    canvas.scrollOffset.y = -top;
+  }
+
+  onResize() {
+    const { canvas, scroller } = this;
+    scroller.firstUpdate = true;
+    requestAnimationFrame(() => {
+      canvas.updateDomDimensions(true);
+    });
   }
 }
 
--- src/webgl-canvas.js
+++ src/webgl-canvas.js
@@ -1,31 +1,66 @@
+import { Vec2 } from 'ogl';
 import OGLApp from './base/ogl-app';
-//import imageTest from '../assets/image_test.jpg';
 import ImagePlane from './image-plane';
 
 export default class WebGLCanvas {
-  constructor({ imageElements = [] }) {
-    this.imagePlanes = [];
-    this.imageElements = imageElements;
+  constructor() {
+    this.scrollingObjects = [];
+    this.scrollOffset = new Vec2();
     this.app = new OGLApp({
       canvas: document.querySelector('#gl-canvas'),
       onRenderCallback: this.onRender.bind(this),
       onResizeCallback: this.onResize.bind(this),
       orbitControls: false,
     });
-    this.initImagePlanes();
     this.setup();
     this.app.start();
   }
 
-  initImagePlanes() {
-    const { gl, scene, imagePlanes, imageElements } = this;
-    imageElements.forEach(el => {
-      // const plane = new ImagePlane(gl, { src: imageTest });
-      // plane.size.set(100, 100);
-      // plane.position.set(100, 100);
-      // plane.scale.set(1.5, 1.5);
-      // plane.setParent(scene);
-      // imagePlanes.push(plane);
+  addDomImages(images) {
+    const { app, scrollingObjects } = this;
+    images.forEach(el => {
+      const plane = new ImagePlane(app.gl, { src: el.src });
+      plane.setParent(app.scene);
+      scrollingObjects.push({
+        mesh: plane,
+        domElement: el,
+        initialPosition: new Vec2(),
+      });
+    });
+    this.updateDomDimensions();
+  }
+
+  updateDomDimensions(recomputeStyle = false) {
+    /*
+      TODO: considering the parent offset (HTML.OffsetParent) recursively
+      could handle edge cases where nesting and borders displace the
+      children positions.
+    */
+    const { scrollingObjects } = this;
+    scrollingObjects.forEach(obj => {
+      const { mesh, domElement, initialPosition } = obj;
+      const { width, height, top, left } = domElement.getBoundingClientRect();
+      const x = left + window.scrollX;
+      const y = top + window.scrollY;
+      mesh.size.set(width, height);
+      initialPosition.set(x, y);
+
+      /*
+        Handle clases where elements have CSS box-size property set to border-box
+        HTMLElement.getBoundingClientRect wont report these offsets.
+      */
+      if (recomputeStyle) {
+        const style = window.getComputedStyle(domElement);
+        if ( style.boxSizing === 'border-box') {
+          const { paddingLeft, paddingRight, paddingTop, paddingBottom } = style;
+          const wPad = Number.parseFloat(paddingLeft) + Number.parseFloat(paddingRight);
+          const hPad = Number.parseFloat(paddingTop) + Number.parseFloat(paddingBottom);
+          const w = width - wPad;
+          const h = height - hPad;
+          mesh.size.set(w, h);
+          initialPosition.set(x + wPad / 2, y + hPad / 2);
+        }
+      }
     });
   }
 
@@ -40,9 +75,11 @@ export default class WebGLCanvas {
   }
 
   onRender({ elapsedTime, delta, scene, camera, renderer }) {
-    const { imagePlanes } = this;
-    for (let i = 0; i < imagePlanes.length; i++) {
-      imagePlanes[i].update({ render, camera });
+    const { scrollingObjects, scrollOffset } = this;
+    for (let i = 0; i < scrollingObjects.length; i++) {
+      const { mesh, initialPosition } = this.scrollingObjects[i];
+      mesh.position.copy(initialPosition).add(scrollOffset)
+      mesh.update({ renderer, camera });
     }
     renderer.render({ scene, camera });
   }
