fbec1221fa78d4a697101bab77cffb75a4888237
Iván Juárez Núñez <ivan@blindgazer.com>
2021-04-16 13:04:22 -0500

    pending files

--- src/shaders/fragment.glsl
+++ src/shaders/fragment.glsl
@@ -4,7 +4,10 @@ uniform float uTime;
 uniform sampler2D tMap;
 varying vec2 vUv;
 varying vec3 vNormal;
+
 void main() {
     vec3 normal = normalize(vNormal);
-    gl_FragColor = texture2D(tMap, vUv);
+    vec4 texel = texture2D(tMap, vUv);
+    texel.rgb *= vec3(1.0, 0.5, 0.5);
+    gl_FragColor = texel;
 }
